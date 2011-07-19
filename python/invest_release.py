aa0742626be76ad35b8ef9bf28a8f04c97b790dc
jdouglass <jdouglass@stanford.edu>
2011-07-19 12:08:36 -0700

    Fixes issue 4
    Python script takes existing template files and inserts the necessary information.

--- python/invest_release.py
+++ python/invest_release.py
@@ -1,19 +1,27 @@
 #! /usr/bin/python
 import sys
+import array
 
 dl_version = sys.argv[1] #"2.2"
 dl_location = sys.argv[2] #"http://invest-natcap.googlecode.com/files/InVEST_2.2_beta-upgrade.exe"
 
-f = open("InVEST_download.template")
-output = open("download.html", "w")
-#read the desired file in to a string
-page = f.read()
-f.close()
+files = [[],[]]
+files[0] = ['InVEST_download.template', 'download.html']
+files[1] = ['homePage.template', 'home04.html']
 
-#use a builtin python search/replace function to replace the substrings with correct data
-page = page.replace("{{{{INVEST_VERSION}}}}", dl_version)
-page = page.replace("{{{{INVEST_DL_LOC}}}}", dl_location)
+for i in xrange(0,2):
+    f = open(files[i][0])
+    output = open(files[i][1], "w")
+    
+    #read the desired file in to a string and then close it
+    page = f.read()
+    f.close()
+    
+    #use a builtin python search/replace function to replace the substrings with correct data
+    page = page.replace("{{{{INVEST_VERSION}}}}", dl_version)
+    page = page.replace("{{{{INVEST_DL_LOC}}}}", dl_location)
 
-#print the string to the correct file.
-output.write(page) 
+    #print the string to the correct file.
+    output.write(page) 
+    output.close();
 
