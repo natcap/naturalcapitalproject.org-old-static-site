c785c637506d5673826d1cbb8db03b5cb0d5d07f
jdouglass <jdouglass@stanford.edu>
2011-07-18 18:07:33 -0700

    Added template files and outlined structure of python script

---
+++ python/invest_release.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+
+#for each file,
+#read the desired file in to a string
+#use a builtin python search/replace function to replace the substrings with correct data
+#print the string to the correct file.
