f6d02c2d619264db723790109f72c6f318a16bdd
jdouglass <jdouglass@stanford.edu>
2011-07-19 14:31:28 -0700

    Added simple command-line argument error handling in the python script and doit.sh

--- python/doit.sh
+++ python/doit.sh
@@ -2,4 +2,11 @@
 
 ./invest_release.py $1 $2
 
-scp ./*.html naturalcapitalproject.org
+#$? checks the error code of the last executed command
+if [ $? != 0 ]; then
+	echo "release script returned with nonzero exit code";
+	echo "exiting";
+	exit 1;
+fi
+
+scp ./*.html naturalcapitalproje@naturalcapitalproject.org:~/
