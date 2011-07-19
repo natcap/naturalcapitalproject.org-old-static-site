d2a4983ce062b533aaeeb6589ec7cbf05b82f08e
jdouglass <jdouglass@stanford.edu>
2011-07-18 18:31:17 -0700

    Initial commit of invest release script.
    Includes basic command line arguments, but is currently limited to download.html

--- python/invest_release.py
+++ python/invest_release.py
@@ -1,6 +1,19 @@
 #! /usr/bin/python
+import sys
 
-#for each file,
+dl_version = sys.argv[1] #"2.2"
+dl_location = sys.argv[2] #"http://invest-natcap.googlecode.com/files/InVEST_2.2_beta-upgrade.exe"
+
+f = open("InVEST_download.template")
+output = open("download.html", "w")
 #read the desired file in to a string
+page = f.read()
+f.close()
+
 #use a builtin python search/replace function to replace the substrings with correct data
+page = page.replace("{{{{INVEST_VERSION}}}}", dl_version)
+page = page.replace("{{{{INVEST_DL_LOC}}}}", dl_location)
+
 #print the string to the correct file.
+output.write(page) 
+
