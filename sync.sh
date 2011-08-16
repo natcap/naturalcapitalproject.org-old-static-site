364ce98c4ccce6fa197004797b1693841ca688e6
richpsharp <devnull@localhost>
2011-08-15 22:53:02 -0700

    A synchronize script to rsync to naturalcapitalproject.org.  Should use ssh keys for authentication.

--- sync.sh
+++ sync.sh
@@ -2,6 +2,8 @@
 
 echo 'Make all files readable'
 find ./ -type f -print0 | xargs -0 chmod 644
+echo 'Make all script files executable'
+find ./ -name "*.sh" -print0 | xargs -0 chmod u+x
 
 echo 'Make all directories executable'
 find ./ -type d -print0 | xargs -0 chmod 711
