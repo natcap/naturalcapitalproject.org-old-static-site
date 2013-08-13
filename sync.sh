1c9185d923549fe43e8529b312ad2669ad218555
James Douglass <jdouglass@stanford.edu>
2013-08-13 11:54:42 -0700

    Restoring sync.sh

---
+++ sync.sh
@@ -0,0 +1,12 @@
+echo "Fix this so it checks changes first!"
+#!/bin/bash
+
+echo 'Make all files readable'
+find ./ -type f -print0 | xargs -0 chmod 644
+echo 'Make all script files executable'
+find ./ -name "*.sh" -print0 | xargs -0 chmod u+x
+
+echo 'Make all directories executable'
+find ./ -type d -print0 | xargs -0 chmod 711
+
+rsync -avP --exclude=".hg" ./. naturalcapitalproje@naturalcapitalproject.org:public_html
