6210760d203812abf3ad7b9d9d6e1be2c60ebadc
Richard Sharp <richpsharp@gmail.com>
2014-12-08 15:17:26 -0800

    leaving the old command to sync with hostway in there for reference

--- sync.sh
+++ sync.sh
@@ -8,4 +8,5 @@ find ./ -name "*.sh" -print0 | xargs -0 chmod u+x
 echo 'Make all directories executable'
 find ./ -type d -print0 | xargs -0 chmod 755
 
-rsync -avP --exclude=".hg" ./. naturalcapitalproje@130.211.132.207:/var/www/naturalcapitalproject.org/public_html
+rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.132.207:/var/www/naturalcapitalproject.org/public_html
+#rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@naturalcapitalproject.org:public_html
