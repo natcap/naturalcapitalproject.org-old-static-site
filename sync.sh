310ad6142f52bc6554284c75433b869d1fbdb583
Richard Sharp <richpsharp@gmail.com>
2014-12-08 16:29:27 -0800

    sync back to the new website

--- sync.sh
+++ sync.sh
@@ -8,6 +8,6 @@ find ./ -name "*.sh" -print0 | xargs -0 chmod u+x
 echo 'Make all directories executable'
 find ./ -type d -print0 | xargs -0 chmod 755
 
-#rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.163.122:/var/www/naturalcapitalproject.org/public_html
+rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.163.122:/var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.132.207:/var/www/naturalcapitalproject.org/public_html
-rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@naturalcapitalproject.org:public_html
+#rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@naturalcapitalproject.org:public_html
