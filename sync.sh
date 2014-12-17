f2a699f3923df0a975363a298ae71ed3282423e4
Rich Sharp <richpsharp@gmail.com>
2014-12-16 17:06:43 -0800

    current dir for the script

--- sync.sh
+++ sync.sh
@@ -8,7 +8,9 @@ find ./ -name "*.sh" -print0 | xargs -0 chmod u+x
 echo 'Make all directories executable'
 find ./ -type d -print0 | xargs -0 chmod 755
 
-rsync -avP --delete --exclude=".hg" --delete-excluded ./. /var/www/naturalcapitalproject.org/public_html
+CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+rsync -avP --delete --exclude=".hg" --delete-excluded $CURRENT_DIR/. /var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.163.122:/var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.132.207:/var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@naturalcapitalproject.org:public_html
