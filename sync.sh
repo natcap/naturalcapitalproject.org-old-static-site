c6911b078c7bac9a3cf465c42907f7d55f63e60a
Rich Sharp <richpsharp@gmail.com>
2014-12-16 17:31:42 -0800

    making the permission changes on the website itself, not the current directory

--- sync.sh
+++ sync.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo 'Make all files readable'
-find ./ -type f -print0 | xargs -0 chmod 644
-echo 'Make all script files executable'
-find ./ -name "*.sh" -print0 | xargs -0 chmod u+x
-
-echo 'Make all directories executable'
-find ./ -type d -print0 | xargs -0 chmod 755
-
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo 'Make all files readable'
+find $CURRENT_DIR -type f -print0 | xargs -0 chmod 644
+echo 'Make all script files executable'
+find $CURRENT_DIR -name "*.sh" -print0 | xargs -0 chmod u+x
+echo 'Make all directories executable'
+find $CURRENT_DIR -type d -print0 | xargs -0 chmod 755
+
 
 rsync -avP --delete --exclude=".hg" --delete-excluded $CURRENT_DIR/. /var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.163.122:/var/www/naturalcapitalproject.org/public_html
