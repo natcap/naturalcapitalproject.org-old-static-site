6b4528fc0795414fba7d74168d92536978e4ca4c
Richard Sharp <richpsharp@gmail.com>
2015-05-18 15:57:00 -0700

    pushing to wrong directory

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ find $CURRENT_DIR -name "*.sh" -print0 | xargs -0 chmod u+x
 echo 'Make all directories executable'
 find $CURRENT_DIR -type d -print0 | xargs -0 chmod 755
 
-rsync -avP --exclude=".hg" --delete-excluded $CURRENT_DIR/. /var/www/naturalcapitalproject.org/public_html
+rsync -avP --exclude=".hg" $CURRENT_DIR/. /var/www
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.163.122:/var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.132.207:/var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@naturalcapitalproject.org:public_html
