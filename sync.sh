b30504c1b0299aa301a6c73fff27a7693f7710ae
Richard Sharp <richpsharp@gmail.com>
2015-05-20 13:46:37 -0700

    excluding .htaccess

--- sync.sh
+++ sync.sh
@@ -9,6 +9,6 @@ find $CURRENT_DIR -name "*.sh" -print0 | xargs -0 chmod u+x
 echo 'Make all directories executable'
 find $CURRENT_DIR -type d -print0 | xargs -0 chmod 755
 
-rsync -avP --exclude=".hg" $CURRENT_DIR/. /var/www
+rsync -avP --exclude=".hg" --exclude=".htaccess" $CURRENT_DIR/. /var/www
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@130.211.132.207:/var/www/naturalcapitalproject.org/public_html
 #rsync -avP --delete --exclude=".hg" --delete-excluded ./. naturalcapitalproje@naturalcapitalproject.org:public_html
